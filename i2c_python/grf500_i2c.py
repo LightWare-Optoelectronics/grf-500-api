d35ee5e83f8cae77790182754d4813fb93f4ea68
Robert Gowans <th3mag3@gmail.com>
2025-12-01 15:45:24 +0200

    Updated license to MIT-0

--- i2c_python/grf500_i2c.py
+++ i2c_python/grf500_i2c.py
@@ -8,14 +8,6 @@ This module provides a Python interface to the GRF-500 sensor over I2C interface
 
 License: MIT No Attribution (MIT-0)
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
 deal in the Software without restriction, including without limitation the
